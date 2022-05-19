3a8c7dd3bda7a26e0086fa54c2283e3821562eee
Andrew Stoneman <andrew@pumpkinpirate.info>
2022-05-18 21:04:01 -0700

    Cooling puzzle first prototype

--- js/about.4a713b3d.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),s={class:"about"},c=Object(e["g"])("h1",null,"This is an about page",-1),o=[c];function u(n,t){return Object(e["s"])(),Object(e["f"])("div",s,o)}const b={};b.render=u;t["default"]=b}}]);
-//# sourceMappingURL=about.4a713b3d.js.map
